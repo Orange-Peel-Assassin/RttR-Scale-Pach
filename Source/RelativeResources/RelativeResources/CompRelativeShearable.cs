6bf5aabeaec7dd8b90d0890aeaf282d68b73aae0
Orange-Peel-Assassin <56128431+Orange-Peel-Assassin@users.noreply.github.com>
2021-10-05 20:48:35 -0700

    3rd First

---
+++ Source/RelativeResources/RelativeResources/CompRelativeShearable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+
+namespace RelativeResources
+{
+    class CompRelativeShearable : CompShearable
+    {
+        public CompProperties_RelativeShearable Props => (CompProperties_RelativeShearable)this.props;
+
+        private Pawn ParentPawn => (Pawn)this.parent;
+        protected override int ResourceAmount => (int)Math.Round(base.ResourceAmount * ParentPawn.BodySize);
+    }
+    public class CompProperties_RelativeShearable : CompProperties_Shearable
+    {
+        public CompProperties_RelativeShearable()
+        {
+            this.compClass = typeof(CompRelativeShearable);
+        } 
+
+    }
+}
